4d4dd8d962ad92ff05f285ded15e177ed481dff3
Michael Rao <jmichaelrao@gmail.com>
2021-08-27 10:06:52 -0500

    update search books to reflect correct typedefs names

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -10,7 +10,7 @@ import {
 } from "react-bootstrap";
 
 import Auth from "../utils/auth";
-import { searchGoogleBooks } from "../utils/API";
+import { saveBook, searchGoogleBooks } from "../utils/API";
 import { saveBookIds, getSavedBookIds } from "../utils/localStorage";
 
 import { SAVE_BOOK } from "../utils/mutations";
@@ -21,12 +21,9 @@ const SearchBooks = () => {
   const [searchedBooks, setSearchedBooks] = useState([]);
   // create state for holding our search field data
   const [searchInput, setSearchInput] = useState("");
-
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
-
   const [saveBook, { error }] = useMutation(SAVE_BOOK);
-
   useEffect(() => {
     return () => saveBookIds(savedBookIds);
   });
@@ -42,7 +39,7 @@ const SearchBooks = () => {
     try {
       const response = await searchGoogleBooks(searchInput);
 
-      if (error) {
+      if (!response.ok) {
         throw new Error("something went wrong!");
       }
 
