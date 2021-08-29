4a5024e128db195f413c8619c9b01f9e5f338030
Michael Rao <jmichaelrao@gmail.com>
2021-08-29 23:23:15 +0200

    update auth page to correct resolver access

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -4,37 +4,33 @@ const secret = "mysecretsshhhhh";
 const expiration = "2h";
 
 module.exports = {
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
-
   authMiddleware: function ({ req }) {
     //allows token to be sent via req.body, req.query, or headers
-
     let token = req.body.token || req.query.token || req.headers.authorization;
-    // let token = req.query.token || req.headers.authorization;
-
-    //separate "Bearer" from "<tokenvalue>"
+    // Split the token string into an array and return actual token
     if (req.headers.authorization) {
       token = token.split(" ").pop().trim();
     }
 
-    //if no token, return request object as is
+    //if no token, return req object
     if (!token) {
       return req;
     }
 
     try {
-      //decode and attach user data to request object
+      // if token can be verified, add the decoded user's data to the request so it can be accessed in the resolver
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log("Invalid token");
     }
 
-    //return updated request object
+    // return the request object so it can be passed to the resolver as `context`
     return req;
+  },
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
